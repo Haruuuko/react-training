f9342be4f36e8d0df9d4dfa630b285c7ba5e5c73
WangQing <wangqing6993@outlook.com>
2017-01-04 17:04:53 +0800

    Init

---
+++ src/nm/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App';
+
+function run(){
+  render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+run();
